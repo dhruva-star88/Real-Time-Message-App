a1230ea05df6c6c3381b7f8495a2f613c90f5b2a
Dhruva <dhruvatej6565@gmail.com>
2025-01-23 20:41:45 +0530

    Created API request for updating read status

--- frontend/src/api/MessageRequest.js
+++ frontend/src/api/MessageRequest.js
@@ -1,6 +1,7 @@
 import axios from "axios"
 
-const API = axios.create({baseURL: 'http://localhost:5000'})
+const API = axios.create({baseURL: 'http://localhost:5000'});
 
-export const getMessages = (id) => API.get(`/message/${id}`)
-export const addMessage = (data) => API.post('/message/', data)
+export const getMessages = (id) => API.get(`/message/${id}`);
+export const addMessage = (data) => API.post('/message/', data);
+export const updateReadStatus = (msg_id) => API.patch(`/message/read/${msg_id}`, {read: true});
